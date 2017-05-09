e6aaa973a5a438e0a6598286d84b67d496909150
Emanuel Damian Paz <emanuel.paz@despegar.com>
2017-05-08 21:07:12 -0300

    Modificaion de camar,a y nivelacion de prefabs

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+
+    public float speed;
+
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * speed ;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed ;
+        }
+
+    }
+
+}
